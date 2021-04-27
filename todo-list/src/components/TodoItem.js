50c32719833d63c53c1c6a77eaa27563adb063c8
lengoctrong <ntrong0603.dgt@gmail.com>
2021-04-27 13:55:52 +0700

    update new todolist

---
+++ todo-list/src/components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class TotoItem extends Component
+{
+    render()
+    {
+        return (
+            <div className="TodoItem">
+                <p>Đi chợ</p>
+            </div>
+        );
+    }
+}
+
+export default TotoItem;
